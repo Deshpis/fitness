f03c1b6943bc3bca870239e60359514e3c950129
Илья <deshpis@yandex.ru>
2022-08-17 18:51:31 +0400

    Выполнена часть работы

--- source/js/main.js
+++ source/js/main.js
@@ -13,6 +13,81 @@ window.addEventListener('DOMContentLoaded', () => {
   // Modules
   // ---------------------------------
 
+  // Инициализация видео
+
+  function findVideos() {
+    let videos = document.querySelectorAll('.video');
+
+    for (let i = 0; i < videos.length; i++) {
+      setupVideo(videos[i]);
+    }
+  }
+
+  function setupVideo(video) {
+    let link = video.querySelector('.video__link');
+    let button = video.querySelector('.video__button');
+    let href = link.href.slice(32);
+
+    button.addEventListener('click', () => {
+      let iframe = createIframe(href);
+
+      link.remove();
+      button.remove();
+      video.appendChild(iframe);
+      video.classList.add('video--enabled');
+    });
+
+    link.removeAttribute('href');
+  }
+
+  function createIframe(href) {
+    let iframe = document.createElement('iframe');
+
+    iframe.setAttribute('allowfullscreen', '');
+    iframe.setAttribute('allow', 'autoplay');
+    iframe.setAttribute('src', generateURL(href));
+    iframe.classList.add('video__media');
+
+    return iframe;
+  }
+
+  function generateURL(href) {
+    let query = '?rel=0&showinfo=0&autoplay=1';
+
+    return 'https://www.youtube.com/embed/' + href + query;
+  }
+
+  findVideos();
+
+  // Табы
+
+  const tabs = document.querySelector('[data-tab="container"]');
+
+  const removeTriggerActive = () => {
+    document.querySelectorAll('[data-tab="trigger"]').forEach((child) => child.classList.remove('tabs__trigger--active'));
+  };
+
+  const removeContentActive = () => {
+    document.querySelectorAll('[data-tab="content"]').forEach((child) => child.classList.remove('tabs__content--active'));
+  };
+
+  if (tabs) {
+    const firstTab = tabs.querySelector('[data-tab="trigger"]');
+
+    tabs.querySelectorAll('[data-tab="trigger"]').forEach((item) => {
+      item.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        const id = evt.target.getAttribute('href').replace('#', '');
+        removeTriggerActive();
+        removeContentActive();
+        item.classList.add('tabs__trigger--active');
+        document.getElementById(id).classList.add('tabs__content--active');
+      });
+    });
+
+    firstTab.click();
+  }
+
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,2 +1,30 @@
 // Swiper 7.4.1
-// import './vendor/swiper';
+import './vendor/swiper';
+
+const mobileWidth = window.matchMedia('(max-width:767px)');
+const tabletWidth = window.matchMedia('(max-width:1199px)');
+let numberCoaches = 4;
+let marginRight = 40;
+
+const breakpointChecker = () => {
+  if (tabletWidth.matches) {
+    numberCoaches = 2;
+    marginRight = 30;
+  }
+
+  if (mobileWidth.matches) {
+    numberCoaches = 1;
+  }
+};
+tabletWidth.addListener(breakpointChecker);
+breakpointChecker();
+
+const swiperCoaches = new Swiper('.coaches__swiper', {
+  simulateTouch: false,
+  slidesPerView: numberCoaches,
+  spaceBetween: marginRight,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
